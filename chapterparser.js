f40c382ce37cd41a4a05afb6df9dc18384916845
travis <tsdubose@gmail.com>
2018-03-06 07:41:19 -0500

    Updated the chapter parser to conform to the current parser flow.

--- chapterparser.js
+++ chapterparser.js
@@ -1,66 +1,30 @@
-// const flatten = require('flatten.js');
 //Expects the whole text data array, minus the shaved off raw text, as an input.
-//Returns a promise with an object of the form detailed below. This will need to be cleaned up.
-//TODO: I need to do some type checking and throw errors, look for reasons to reject.
-//TODO: I don't think I'm gathering the right totals here, but it doesn't matter yet.
+//Returns a promise with an object giving the total number of chapters, and the number of paragraphs in each chapter.
+//Note that the total number of chapters is captured as a key and given a value of 1: this is so the length of works can properly be combined later.
 const arrayCounter = require('./arraycounter.js');
 module.exports = function (chapters) {
 	//Inititalize the data structure, so that you don't have to test for it later.
 	let chapterData = {
-		structure : {
-			dialogue: {
-					starts: 0,
-					following: {
-						dialogue: 0,
-						narration: 0
-					},
-					total: 0
-			},
-			narration: {
-				starts: 0,
-				following: {
-					dialogue: 0,
-					narration: 0
-				},
-				total: 0
-			}
-		}
+		chLength: {}
 	};
 
 	return new Promise(function(resolve, reject) {
+			chapterData[chapters.length] = 1;
 			chapterData.chLength = arrayCounter(chapters);
-			chapters.forEach(function (chapter) {
-				chapter.forEach(function (paragraph, index, a) {
-					let current = narrOrDia(paragraph);
-					let next = narrOrDia(a[index + 1]);
+			if (hasProps(chapterData.chLength)) {
+				resolve(chapterData);
+			}
+			else {
+				reject("Could not calculate the chapter data.");
+			}
 
-					if (current) {
-						if (index == 0) {
-							chapterData.structure[current].starts += 1;
-							chapterData.structure[current].total += 1;
-							if (next) {
-								chapterData.structure[current].following[next] += 1;
-							}
-						}
-						else {
-							chapterData.structure[current].total += 1;
-							if (next) {
-								chapterData.structure[current].following[next] += 1;
-							}
-						}
-					}
-				});
-			});
-			resolve(chapterData);
 	});
-
 };
-
-function narrOrDia(paragraph) {
-	if (paragraph && typeof paragraph === "string") {
-		return paragraph[0] === "“" ? "dialogue" : "narration";
+function hasProps(obj) {
+	for(var prop in obj) {
+		if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+			return true;
+		}
 	}
-	else {
-		return null;
-	}
+	return false;
 }
