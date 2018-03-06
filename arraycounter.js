72d17c48401ff35832b8deae442fc28410f81dd4
travis <tsdubose@gmail.com>
2018-03-06 07:41:57 -0500

    Updated arraycounter to use a for loop instead of forEach.

--- arraycounter.js
+++ arraycounter.js
@@ -1,20 +1,19 @@
-//Accepts an array of arrays as input and counts the number of items in each array, returning an object that contains the % of times
+//Accepts an array of arrays as input and counts the number of items in each array, returning an object that contains the number of times
 //each item was found, rounded to 3 decimals.
 /* Ex: {
 totalItems: 95,
-3: 5, <- Indicates that a length of 3 was found five times
+3: 5, <- Indicates that a length of 3 was found 5 times
 4: 6 <- Indicates that a length of 4 was found 6 times etc
 }
 */
-module.exports = function (arrays) {
+module.exports = function(arrays) {
 	let countingData = {};
-	arrays.forEach(function (item) {
-		if (Object.keys(countingData).includes(item.length.toString())) {
-			countingData[item.length] += 1;
+	for (let i = 0; i < arrays.length; i++) {
+		if (Object.keys(countingData).includes(arrays[i].length.toString())) {
+			countingData[arrays[i].length] += 1;
+		} else {
+			countingData[arrays[i].length] = 1;
 		}
-		else {
-			countingData[item.length] = 1;
-		}
-	});
+	}
 	return countingData;
 };
