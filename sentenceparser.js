0116c3295866c26f146baa10f51b4018cdb8e73a
travis <tsdubose@gmail.com>
2018-03-08 10:47:35 -0500

    Rewrote sentence parser module to take multiple types of input
    Module can now take dialogue, narration or dialogue tags.

--- sentenceparser.js
+++ sentenceparser.js
@@ -1,40 +1,47 @@
-//This function expects the array of chapters, and the object of characters and themes as input, flattens the chapters
-//into a one-dimensional array of paragraphs, splits the paragraphs into sentences, analyzes the length of those paragraphs, then parses the grammar of the
-//sentences, replacing characters and themes as appropriate. Returns a promise.
-
-// IDEA: Think about places where the code here can be reused for the dialogue parser.
-// IDEA: I think I might create an object prototype of "grammar holder" in order to better use some of the setter/getter behavior on the objects.
-// TODO: Find a place to reject.
-// TODO: There's mid-paragraph dialogue sneaking in that I need to deal with eventually.
-const flatten = require('./flatten.js');
+/*The function that sorts the sentences, no matter their category, into their proper places.
+Expects the array of semantic blocks and the character and theme data as its two
+arguments.*/
+// TODO: Need to think about the commas surrounding dialogue tags, probably take them all out and then put them in manually.
+const lowerCase = /^[a-z]$/;
 const rita = require('rita');
-const arrayCounter = require('./arraycounter.js');
+const sortParts = require('./objectsorter.js');
 const arrayIncludes = require('./arrayincludes.js');
-const sortSpeech = require('./partsofspeechsorter.js');
-
-module.exports = function(chapters, charAndTheme) {
-	let sentenceData = {};
-	return new Promise(function(resolve) {
-		//Flatten the chapters, filter out the dialogue and split each paragraph into an array of sentences for parsing.
-		// console.log(chapters);
-		let narration = flatten(chapters)
-			.filter((paragraph) => paragraph[0] !== "“")
-			.map((item) => {
-				let cleanPara = item.replace(/\r\n/g, " ");
-				return rita.splitSentences(cleanPara);
-			});
-		//Add the paragraph length data to the data structure.
-		sentenceData.paraLength = arrayCounter(narration);
-		//Take each sentence, tokenize it, then test to see if it includes any characters or themes.
-		narration.forEach(function(paragraph) {
-			paragraph.forEach(function(sentence) {
-				let tokens = rita.tokenize(sentence);
-				let posTags = rita.getPosTags(sentence);
-				const themeKeys = Object.keys(charAndTheme);
-				const flatThemes = flatten(
-					themeKeys.map(function(key) {
-						return charAndTheme[key];
-					}));
+const hasProps = require('./hasprops.js');
+const flatten = require('./flatten.js');
+const titles = ["Adm", "Capt", "Cmdr", "Col", "Dr", "Gen", "Gov", "Lt", "Maj", "Messrs", "Mr", "Mrs", "Ms", "Prof", "Rep", "Reps", "Rev", "Sen", "Sens", "Sgt", "Sr", "St", "Miss"];
+module.exports = function(semBlocks, charAndTheme) {
+	var sentenceData = {
+		narration: {},
+		dialogue: {},
+		diaTag: {}
+	};
+	//Create an array of theme keys and an array with all the words inside the theme
+	//object for later testing.
+	const themeKeys = Object.keys(charAndTheme);
+	const flatThemes = flatten(
+		themeKeys.map(function(key) {
+			return charAndTheme[key];
+		}));
+	return new Promise(function(resolve, reject) {
+		//Create a variable to hold the sentence type, so that the sentences can be
+		//properly sorted. Also create a placeholder to use in the next for loop to
+		//improve readability.
+		var sentType, thisBlock;
+		for (let i = 0; i < semBlocks.length; i++) {
+			thisBlock = semBlocks[i];
+			if (thisBlock[0][0] == "“") {
+				sentType = "dialogue";
+			} else if (lowerCase.test(thisBlock[0][0])) {
+				sentType = "diaTag";
+			} else {
+				sentType = "narration";
+			}
+			//Run through the semantic block and categorize each sentence.
+			for (let n = 0; n < thisBlock.length; n++) {
+				var cleanItem = thisBlock[n].replace(/[“”]/g, "");
+				//Take each sentence, tokenize it, then test to see if it includes any characters or themes.
+				let tokens = rita.tokenize(cleanItem);
+				let posTags = rita.getPosTags(cleanItem);
 				//Check to see if the tokens include any of the characters or themes. If so, append them to the POS tags array.
 				if (arrayIncludes(tokens, flatThemes)) {
 					tokens.forEach(function(word, index) {
@@ -47,26 +54,38 @@ module.exports = function(chapters, charAndTheme) {
 					});
 				}
 				//The temporary solution to the Mr. problem: check against a hard-wired list of possibilities stolen from RiTa,
-				//and then replace the posTags with that exact title, also grabbing and periods that come next.
-				tokens.forEach(function (word, index) {
-					const titles = ["Adm", "Capt", "Cmdr", "Col", "Dr", "Gen", "Gov", "Lt", "Maj", "Messrs", "Mr", "Mrs", "Ms", "Prof", "Rep", "Reps", "Rev", "Sen", "Sens", "Sgt", "Sr", "St"];
-					if (titles.includes(word)) {
-						posTags[index] = word;
-						if (posTags[index + 1] == ".") {
-							posTags[index] += ".";
-							posTags.splice(index + 1, 1);
+				//and then replace the posTags with that exact title, also grabbing any periods that come next.
+				for (let t = 0; t < tokens.length; t++) {
+					if (titles.includes(tokens[t])) {
+						posTags[t] = tokens[t];
+						if (posTags[t + 1] == ".") {
+							posTags[t] += ".";
+							posTags.splice(t + 1, 1);
 						}
 					}
-				});
-				//Now it's time to actually start sorting the sentences into probabilities. Ah!
-				const sentenceLengths = Object.keys(sentenceData);
-				if (!sentenceLengths.includes(posTags.length.toString())) {
-					sentenceData[posTags.length] = {};
-					sentenceData[posTags.length].starts = {};
 				}
-				sentenceData[posTags.length] = sortSpeech(posTags, sentenceData[posTags.length]);
-			});
-		});
-		resolve(sentenceData);
+				//Sort the sentences into probabilities. Check to see if that sentence type has a sentence of that length and then call the function that sorts probabilities.
+				var sentenceLengths = Object.keys(sentenceData[sentType]);
+				if (!sentenceLengths.includes(posTags.length.toString())) {
+					sentenceData[sentType][posTags.length] = {};
+					sentenceData[sentType][posTags.length].starts = {};
+				}
+				sentenceData[sentType][posTags.length] = sortParts(posTags, sentenceData[sentType][posTags.length]);
+			}
+		}
+		for (var key in sentenceData) {
+			if (sentenceData.hasOwnProperty(key)) {
+				if (hasProps(sentenceData[key])) {
+					let resolvedSentenceData = {
+						sentence: sentenceData
+					};
+					resolve(resolvedSentenceData);
+					break;
+				}
+				else {
+					reject("Sentence data could not be calculated.");
+				}
+			}
+		}
 	});
 };
