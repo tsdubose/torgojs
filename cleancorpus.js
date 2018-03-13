a35900e3c45ddc6cb0e84f9a572b41ad167f73eb
travis <tsdubose@gmail.com>
2018-03-13 16:18:15 -0400

    Created corpus cleaning module.
    A small corpus that cleans up the data used to create a dictionary.

---
+++ cleancorpus.js
@@ -0,0 +1,10 @@
+/*This module accepts raw text as an input, and then takes out extraneous line breaks and
+converts all quotes to smart quotes etc.*/
+const smarten = require('smarten.js');
+module.exports = function (rawText) {
+	let smartenedText = smarten(rawText)
+	.replace(/\\n/g, /\[ ]/);
+	return new Promise(function(resolve) {
+		resolve(smartenedText);
+	});
+};
