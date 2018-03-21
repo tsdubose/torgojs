04b926e091c212808cb029da668eb5058ca29ec8
travis <tsdubose@gmail.com>
2018-03-21 08:28:13 -0400

    eslint update to account for async await

--- objectprobabilities.js
+++ objectprobabilities.js
@@ -1,26 +1,29 @@
 //Give an object with numbered properties on it, this function returns a property based on its frequency.
 // FIXME: Right now, this is going to break on empty objects, of which there are many.
-// OPTIMIZE: This needs to work asynchronously as right now it's tying up the event loop.
 module.exports = function (obj) {
 	let probArray = [];
-	for (let key in obj) {
-		if (obj.hasOwnProperty(key)) {
-			if (typeof obj[key] === "number") {
-				for (let i = 0; i < obj[key]; i++) {
-					probArray.push(key);
+	return new Promise(function(resolve, reject) {
+		for (let key in obj) {
+			if (obj.hasOwnProperty(key)) {
+				if (typeof obj[key] === "number") {
+					for (let i = 0; i < obj[key]; i++) {
+						probArray.push(key);
+					}
 				}
-			}
-			else if (typeof obj[key].total === "number") {
-				for (let i = 0; i < obj[key].total; i++) {
-					probArray.push(key);
+				else if (typeof obj[key].total === "number") {
+					for (let i = 0; i < obj[key].total; i++) {
+						probArray.push(key);
+					}
 				}
+				// else {
+				// 	throw new Error("Could not find the probability of a particular feature occurring.")
+				// }
 			}
-			// else {
-			// 	throw new Error("Could not find the probability of a particular feature occurring.")
-			// }
 		}
-	}
-	return probArray[getRandom(0, probArray.length)];
+		console.log(probArray);
+		resolve(probArray[getRandom(0, probArray.length)]);
+	});
+
 };
 
 function getRandom(min, max) {
